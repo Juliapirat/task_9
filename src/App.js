0ce5a8bb622d43a21720fcefd9579a4ff1247c0d
petrashenkii4 <67104418+petrashenkii4@users.noreply.github.com>
2022-09-28 15:13:09 +0300

    Set up imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
 import React, { useState } from "react";
-import { render } from "react-dom";
 import Shop from "./Shop";
 import "./index.css";
 
@@ -28,7 +25,9 @@ export default function App() {
         <div className="flex flex-col bg-white shadow-md px-4 sm:px-6 md:px-8 lg:px-10 py-8 mt-10 rounded-3xl w-50 max-w-md">
         <h2  className='font-medium self-center text-xl sm:text-3xl text-gray-800'>Нужно залогиниться!</h2>
         <div className='mt-5 '>
-        <button  type ="submit" className="flex mt-2 items-center justify-center focus:outline-none text-white text-sm sm:text-base bg-blue-500 hover:bg-blue-600 rounded-2xl py-2 w-full transition duration-150 ease-in" onClick={() => setLogin(true)}>
+        <button  type ="submit" 
+        className="flex mt-2 items-center justify-center focus:outline-none text-white text-sm sm:text-base bg-blue-500 hover:bg-blue-600 rounded-2xl py-2 w-full transition duration-150 ease-in"
+         onClick={() => setLogin(true)}>
           Войти
         </button>
         </div>
